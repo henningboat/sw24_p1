65612429d3d4d564a4e2c7cd0d5e825ea1bd5648
danieleriksen <Daniel.bach.eriksen@gmail.com>
2024-11-14 12:26:47 +0100

    Setup pathfinding files

---
+++ path_finding.c
@@ -0,0 +1,7 @@
+#include "path_finding.h"
+
+#include <stdio.h>
+
+void test_path_finding() {
+    printf("hello path finding");
+}
---
+++ path_finding.h
@@ -0,0 +1,6 @@
+#ifndef PATH_FINDING_H
+#define PATH_FINDING_H
+
+void test_path_finding();
+
+#endif PATH_FINDING_H
