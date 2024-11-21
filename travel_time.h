84a5b57156d92984e7a2d0234b63da24426a8e97
henningsteinbock <talktohenning@gmail.com>
2024-11-21 12:19:41 +0100

    made pointers constant in travel_time.h

--- travel_time.h
+++ travel_time.h
@@ -2,6 +2,6 @@
 #define TRAVEL_TIME_H
 #include "structs.h"
 
-double get_travel_time(Train *train, Connection *connection, double start_speed, double end_speed)
+double get_travel_time(const Train *train, const Connection *connection, double start_speed, double end_speed);
 
 #endif
