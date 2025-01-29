74371aa0d77e9b2bc8781d1de6904a662c7a86fe
henningsteinbock <talktohenning@gmail.com>
2025-01-29 10:39:37 +0100

    last minute fixes after we went through the code together

--- passenger_decision_making.c
+++ passenger_decision_making.c
@@ -2,6 +2,6 @@
 #include "passenger_decision_making.h"
 #include "simulation_loop.h"
 
-int passenger_chooses_train(Passenger passenger, double without_train_time, double with_train_time) {
-  return without_train_time / passenger.train_time_tolerance <= with_train_time;
+int passenger_chooses_train(Passenger passenger, double without_plane_time, double with_plane_time) {
+  return (without_plane_time / passenger.train_time_tolerance <= with_plane_time);
 }
--- passenger_decision_making.h
+++ passenger_decision_making.h
@@ -2,6 +2,6 @@
 #define PASSENGER_DECISION_MAKING_H
 
 #include "structs.h"
-int passenger_chooses_train(Passenger passenger, double without_train_time, double with_train_time);
+int passenger_chooses_train(Passenger passenger, double without_plane_time, double with_plane_time);
 
 #endif
